971c6604861c66db0a9d278517c703c235928cf9
Salog0d <david.salomon.nava11@gmail.com>
2025-02-08 00:34:30 -0600

    Database encryption setup

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,4 +1,7 @@
 class Password < ApplicationRecord
     has_many :user_passwords
     has_many :users, through: :user_passwords
+
+    encrypts :password
+    encrypts :username , deterministic: true
 end
