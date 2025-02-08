ba468bb31d379f8be0adcaffbdf0f94a7dcc3e9b
Salog0d <david.salomon.nava11@gmail.com>
2025-02-08 15:14:31 -0600

    Create and display actions and logic completed

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,36 @@
+class PasswordsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_password, except: [:index, :new, :create]
+  
+    def index
+      @passwords = current_user.passwords
+    end
+  
+    def new
+      @password = Password.new
+    end
+
+    def show
+        set_password
+    end
+  
+    def create
+      @password = current_user.passwords.create(password_params)
+      if @password.persisted?  
+        redirect_to @password, notice: "Password saved successfully."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def password_params
+      params.require(:password).permit(:url, :username, :password)
+    end
+
+    def set_password
+        @password = current_user.passwords.find(params[:id])
+    end
+  end
+  
