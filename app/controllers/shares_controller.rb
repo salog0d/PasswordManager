995adff4785aa4eac7a5e711348b93e92eee2576
Salog0d <david.salomon.nava11@gmail.com>
2025-02-17 00:28:48 -0600

    Added multiple users in passwords functionality, pending destroy action

---
+++ app/controllers/shares_controller.rb
@@ -0,0 +1,33 @@
+class SharesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_password
+
+    def new
+       @users= User.excluding(current_user)
+       @user_password = UserPassword.new
+    end
+
+    def create
+        @user_password = @password.user_passwords.new(user_password_params)
+        if @user_password.save
+            redirect_to @password, notice: "Password shared successfully."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_password
+        @password = current_user.passwords.find(params[:password_id])
+      
+    end
+
+    def user_password_params
+        params.require(:user_password).permit(:user_id)
+    end
+      
+end
